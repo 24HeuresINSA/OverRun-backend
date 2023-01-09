53589a6ac3e7035f960c11b3521cc66b89daf2b6
Phanie Buonomo <phanie.itarilde@gmail.com>
2023-01-09 19:52:24 +0100

    chore: :rotating_light: correct linter configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,15 @@
 module.exports = {
-    root: true,
-    env: {
-      node: true,
-    },
-    plugins: ["@typescript-eslint"],
-    extends: [
-      "eslint:recommended",
-      "plugin:@typescript-eslint/recommended",
-    ],
-    parserOptions: {
-      parser: "@typescript-eslint/parser",
-    },
-  };
-  
+  root: true,
+  env: {
+    node: true,
+  },
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  parserOptions: {
+    parser: "@typescript-eslint/parser",
+  },
+};
